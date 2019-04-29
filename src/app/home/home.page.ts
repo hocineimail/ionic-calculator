f37fdd69d9164b8ffd9a0296861979ff18285777
hocineimail <hocineismail75@gmail.com>
2019-04-29 17:12:59 +0200

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,59 +11,71 @@ export class HomePage {
   Calcul = "";
   state = false
   numberOne() {
+    this.Calcul = this.Calcul + "1"
    this.Result = this.Number + 1
   }
 
   
   NumberTwo() {
+    this.Calcul = this.Calcul + "2"
     this.Result = this.Number + 2
    }
  
   
    
   NumberThree() {
+    this.Calcul = this.Calcul + "3"
     this.Result = this.Number + 3
    }
  
   
    
   NumberFour() {
+    this.Calcul = this.Calcul + "4"
     this.Result = this.Number + 4
    }
  
   
    NumberFive() {
+    this.Calcul = this.Calcul + "5"
     this.Result = this.Number + 5
    }
  
   
    
   NumberSix() {
+    this.Calcul = this.Calcul + "6"
     this.Result = this.Number + 6
    }
  
   
    
   NumberSeven() {
+    this.Calcul = this.Calcul + "7"
     this.Result = this.Number + 7
    }
 
    NumberEight() {
+    this.Calcul = this.Calcul + "8"
     this.Result = this.Number + 8
    }
 
    NumberNine() {
+    this.Calcul = this.Calcul + "9"
     this.Result = this.Number + 9
    }
  
    ButtonAdd() {
-     console.log("plus")
+    this.Calcul = this.Calcul + "+"
      this.Number = this.Result  
    }
 
    Off() {
    
     this.state =  true
+    this.Calcul = ""
+    this.Result = 0
+    this.Number = 0
   }
   On() {
    
