4d869fcc984359e2b32489a040c0c52aca414788
hocineimail <hocineismail75@gmail.com>
2019-04-29 16:51:02 +0200

    Create function of all number

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  Result = 0
+  Number = 0
+  numberOne() {
+   this.Result = this.Number + 1
+  }
 
+  
+  NumberTwo() {
+    this.Result = this.Number + 2
+   }
+ 
+  
+   
+  NumberThree() {
+    this.Result = this.Number + 3
+   }
+ 
+  
+   
+  NumberFour() {
+    this.Result = this.Number + 4
+   }
+ 
+  
+   NumberFive() {
+    this.Result = this.Number + 5
+   }
+ 
+  
+   
+  NumberSix() {
+    this.Result = this.Number + 6
+   }
+ 
+  
+   
+  NumberSeven() {
+    this.Result = this.Number + 7
+   }
+
+   NumberEight() {
+    this.Result = this.Number + 8
+   }
+
+   NumberNine() {
+    this.Result = this.Number + 9
+   }
+ 
+   ButtonAdd() {
+     console.log("plus")
+     this.Number = this.Result  
+   }
 }
